45559acc8e28713447ac673de2fb2e4f4d4decac
Alan Casas <alancasasarevalo@gmail.com>
2018-09-22 19:33:09 +0200

    Some functions were added, rightButton added to open with alert

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Abrir", style: .plain, target: self, action: #selector(openTapped))
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController{
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    @objc func openTapped()  {
+        
+        let activityController = UIAlertController(title: "¿Que pagina quieres abrir?", message: nil, preferredStyle: .actionSheet)
+        let applePageAction = UIAlertAction(title: "apple.com", style: .default, handler: openPage)
+        let hackingPageAction = UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage)
+        let googlePageAction = UIAlertAction(title: "google.com", style: .default, handler: openPage)
+        let cancelPageAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        activityController.addAction(applePageAction)
+        activityController.addAction(hackingPageAction)
+        activityController.addAction(googlePageAction)
+        activityController.addAction(cancelPageAction)
+        
+        activityController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(activityController, animated: true)
+        
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+
+## Introduction
+
+> iOS WebView navigation with browser progress bar example
+
+## Preview
+![](https://github.com/AlanCasasArevalo/Social_media/blob/master/SharePicture.gif)
+
+## This simple app is an example of WebView
+
+This app is simple app to show how works navigation with alerts 
