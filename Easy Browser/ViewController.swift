9f32561a95d3ef6729e80aeebbc56a18c81cac2d
Alan Casas <alancasasarevalo@gmail.com>
2018-09-22 19:43:41 +0200

    Progressive tool bar was added

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +21,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Abrir", style: .plain, target: self, action: #selector(openTapped))
 
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
+
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+
     }
 
     override func didReceiveMemoryWarning() {
